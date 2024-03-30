e9e8e498033ce7b5428d6f3e6395662f717c6096
Ertnad <90501362+Ertnad@users.noreply.github.com>
2024-03-30 05:50:33 +0300

    add while

---
+++ main.py
@@ -0,0 +1,141 @@
+import os
+import mel_parser
+
+
+def main():
+    prog1 = '''
+            if (a) {
+                print(1 + 2);
+                print(0);
+                if (b) {
+                    b = 1;
+                }
+            } else if (b) {
+                print(4);
+            } else if (c) {
+                c = b + c;
+            } else {
+                print(c);
+            }
+        '''
+    prog2 = """
+        a = 5;
+        b = !!a;
+    """
+    prog3 = """
+        a = 10;
+        b = 20;
+        when{
+            (a < 90)->{
+                print(a)
+                a = 2
+            }
+            in 40 ..10->print(b)
+        }
+    """
+    prog4 = """
+            a = 10;
+            b = 20;
+            when(a){
+                (a < 90)->{
+                    print(a)
+                    a = 2
+                }
+                in 10..30->print(b)
+            }
+        """
+    prog5 = """
+        val a: Int = 1
+    """
+    prog15 = """
+        var a: Int = 1
+    """
+    prog6 = """
+            for(i = 8; i < 9; i = i + 1) {
+                print(i)
+            }
+        """
+    prog7 = """
+                for(; i < 9;) {
+                    print(i)
+                    i = i + 1
+                }
+            """
+    prog8 = """
+            a = 5 + 6
+            a = 5 - 6
+            a = 5 * 6
+            a = 5 / 6
+            a = 5 >= 6
+            a = 5 <= 6
+            a = 5 > 6
+            a = 5 < 6
+            a = 5 == 6
+            a = 5 != 6
+            a = 5 && 6
+            a = 5 || 6
+            a = !!a
+        """
+    prog10 = """
+        when(a + b){
+            a -> println(b)
+            b -> println(a)
+            a + 5 -> println(a + 5)
+            in 10..12 -> { a = a + 1 }
+            !in 10..12 -> { isEnable = 2 }
+            else -> { a = 3 }
+        }
+    """
+    prog11 = """
+        if (a > 3) {
+            print(1);
+            a = 3;
+        }
+    """
+    prog12 = """
+        c = when(a) {
+            1 -> a
+            2 -> a + 1
+            else -> a + 1
+        }
+    """
+    prog13 = """
+        when{
+            (b > 10) -> println(b)
+            (a > 10) -> println(a)
+            else -> println(c)
+        }
+    """
+    prog14 = """
+        c = if (a > b) a else if (a == b) b else d
+    """
+    prog16 = """
+        fun sum():Int {
+            for(i = 8; i < 9; i = i + 1) {
+                print(i)
+            }
+        }
+        """
+    prog17 = """
+    fun sum(a: int, b: int) : int {
+        var result: int = 0
+        for(i = 0; i < a; i = i + 1) {
+            result = result + b
+        }
+        return result
+    }
+    """
+    while_check = """
+        var a: Int = 1
+        while (a < 1) {
+            print(a)
+            a = a + 1
+    }
+    """
+
+    prog = mel_parser.parse(while_check)
+    print(*prog.tree, sep=os.linesep)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mel_parser.py
@@ -0,0 +1,168 @@
+from contextlib import suppress
+import inspect
+
+import pyparsing as pp
+from pyparsing import pyparsing_common as ppc
+
+from mel_ast import *
+
+
+def _make_parser():
+    LPAR, RPAR = pp.Literal('(').suppress(), pp.Literal(')').suppress()
+    LBRACE, RBRACE = pp.Literal('{').suppress(), pp.Literal('}').suppress()
+    COMMA, SEMI, POINT = pp.Literal(',').suppress(), pp.Literal(';').suppress(), pp.Literal('.').suppress()
+    ASSIGN = pp.Literal('=')
+    MULT, DIV, MOD = pp.Literal('*'), pp.Literal('/'), pp.Literal('%')
+    PLUS, MINUS = pp.Literal('+'), pp.Literal('-')
+
+    GE, LE, GT, LT = pp.Literal('>='), pp.Literal('<='), pp.Literal('>'), pp.Literal('<')
+    EQUALS, NOTEQUALS = pp.Literal('=='), pp.Literal('!=')
+    LOGIC_AND, LOGIC_OR = pp.Literal('&&'), pp.Literal('||')
+    NOT = pp.Literal('!')
+    OPERATOR = pp.Literal('->').suppress()
+    COLON = pp.Literal(':').suppress()
+
+    IF = pp.Keyword('if').suppress()
+    ELSE = pp.Keyword('else').suppress()
+    WHILE = pp.Keyword('while').suppress()
+    WHEN = pp.Keyword('when').suppress()
+    FOR = pp.Keyword('for').suppress()
+    IN = pp.Keyword('in').suppress()
+
+    VAL = pp.Keyword('val')
+    VAR = pp.Keyword("var")
+    FUN = pp.Keyword("fun")
+
+    keywords = IF | ELSE | WHILE | WHEN | FOR | IN | VAL | VAR | FUN
+
+    int_num = ppc.number.copy().setName("int_num")
+    num = ppc.fnumber.copy().setName('num')
+    ident = (~keywords + ppc.identifier.copy()).setName('ident')  # ~ переопределён для парсера и означает не keywords
+    type_ = ident.copy().setName('type')
+
+    in_ = pp.Forward()
+    expr = pp.Forward()
+    params = pp.Optional(expr + pp.ZeroOrMore(COMMA + expr))
+    call = ident + LPAR + params + RPAR
+    group = call | ident | num | LPAR + expr + RPAR | in_
+    not_ = pp.Forward().setName('unary')
+    not_ << (NOT + (not_ | group))
+    not_or_group = not_ | group
+    mult = (not_or_group + pp.ZeroOrMore((MULT | DIV | MOD) + not_or_group)).setName('binary')
+    add = (mult + pp.ZeroOrMore((PLUS | MINUS) + mult)).setName('binary')
+    compare = (add + pp.Optional((GE | LE | GT | LT | EQUALS | NOTEQUALS) + add)).setName('binary')
+    logic_and = (compare + pp.ZeroOrMore(LOGIC_AND + compare)).setName('binary')
+    logic_or = (logic_and + pp.ZeroOrMore(LOGIC_OR + logic_and)).setName('binary')
+
+    expr << logic_or
+
+    stmt = pp.Forward()
+    empty_stmt = pp.Group(pp.empty).setName("stmt_list")
+    stmt_or_empty = stmt | empty_stmt
+
+    empty_expr = pp.Group(pp.empty).setParseAction(lambda s, loc, tocs: NumNode(1))
+    expr_or_empty = expr | empty_expr
+
+    def var_inner_parse_action(s, loc, tocs):
+        const = str(tocs[0]) == 'val'
+        return VarDecl(const, tocs[1], tocs[2], tocs[3]) if len(tocs) == 4 else VarDecl(const, tocs[1], tocs[2], None)
+
+    var_inner = ((VAR | VAL) + ident + COLON.suppress() + ident + pp.Optional(ASSIGN.suppress() + expr)).setParseAction(
+        var_inner_parse_action)
+
+    if_ = pp.Forward()
+    when = pp.Forward()
+    assign = ident + ASSIGN.suppress() + (expr | if_ | when)
+
+    if_ << ((IF + LPAR + expr + RPAR + stmt + pp.Optional(ELSE + stmt))
+            | (IF + LPAR + expr + RPAR + (ident | expr) + ELSE + (ident | expr | stmt)))
+
+    while_ = pp.Forward()
+
+    # Определение цикла while с возможностью содержать одно выражение или блок выражений в фигурных скобках
+    while_ << (WHILE + LPAR + expr + RPAR + (stmt | (LBRACE + stmt + RBRACE))).setName("while")
+
+    for_ = FOR + LPAR + stmt_or_empty + SEMI + expr_or_empty + SEMI + stmt_or_empty + RPAR + stmt
+
+    in_ << IN + int_num + POINT + int_num
+
+    when_expr = (expr | in_) + OPERATOR + (expr | stmt)
+    when << (WHEN + pp.Optional(LPAR + expr + RPAR) + LBRACE + pp.OneOrMore(when_expr)
+             + pp.Optional(ELSE + OPERATOR + (expr | stmt)) + RBRACE)
+
+    stmt_list = pp.Forward()  # объявляем
+
+    fun_decl = (FUN + ident + LPAR + RPAR + COLON + type_ + LBRACE + stmt_list + RBRACE).setName('func')
+
+    stmt << (
+            call |
+            assign |
+            if_ |
+            for_ |
+            when |
+            var_inner |
+            fun_decl |
+            while_ |
+            LBRACE + stmt_list + RBRACE
+    )
+
+    stmt_list << pp.ZeroOrMore(stmt + pp.Optional(SEMI))  # переопределяем
+
+
+    program = stmt_list.ignore(pp.cStyleComment).ignore(pp.dblSlashComment) + pp.StringEnd()
+
+
+    start = program
+
+    def set_parse_action_magic(rule_name: str, parser: pp.ParserElement) -> None:
+        if rule_name == rule_name.upper():
+            return
+        if getattr(parser, 'name', None) and parser.name.isidentifier():
+            rule_name = parser.name
+        if rule_name == 'binary':
+            def bin_op_parse_action(s, loc, tocs):
+                node = tocs[0]
+                for i in range(1, len(tocs) - 1, 2):
+                    node = BinOpNode(BinOp(tocs[i]), node, tocs[i + 1])
+                return node
+
+            parser.setParseAction(bin_op_parse_action)
+        elif rule_name == 'unary':
+            def un_op_parse_action(s, loc, tocs):
+
+                return UnOpNode(UnOp(tocs[0]), tocs[1])
+
+            parser.setParseAction(un_op_parse_action)
+        elif rule_name == 'func':
+            def func_parse_action(s, loc, tocs):
+                return FunDecl(tocs[1], tocs[2])
+
+            parser.setParseAction(func_parse_action)
+
+        elif rule_name == 'while':
+            def while_parse_action(s, loc, tocs):
+                return WhileNode(tocs[2], tocs[4])
+            parser.setParseAction(while_parse_action)
+
+        else:
+            cls = ''.join(x.capitalize() for x in rule_name.split('_')) + 'Node'
+            with suppress(NameError):
+                cls = eval(cls)
+                if not inspect.isabstract(cls):
+                    def parse_action(s, loc, tocs):
+                        return cls(*tocs)
+
+                    parser.setParseAction(parse_action)
+
+    for var_name, value in locals().copy().items():
+        if isinstance(value, pp.ParserElement):
+            set_parse_action_magic(var_name, value)
+
+    return start
+
+
+parser = _make_parser()
+
+
+def parse(prog: str) -> StmtListNode:
+    return parser.parseString(str(prog))[0]
